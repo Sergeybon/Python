6b1b248ebced5c2c11c4e7d4ec0a9405ab480ca6
Sergey <sbondar05@gmail.com>
2021-10-23 20:06:03 +0300

    add homework 3.2

--- Homework_3_python/DZ3_2/dz3_2.py
+++ Homework_3_python/DZ3_2/dz3_2.py
@@ -1,15 +1,23 @@
 #def nFile(testFile):
 def upperAndLower(filename):
     with open(filename) as f:
-        c = 0
-        for line in f:
-            varUpp = line.upper()
-            f = open('files_outputUppper.txt', 'w')  # открытие в режиме записи
-            f.write(varUpp + '\n')  # запись в файл
-            varLow = line.lower()
-            f = open('files_outputLower.txt', 'w')  # открытие в режиме записи
-            f.write(varLow + '\n')  # запись в файл
 
+        for i in f:
+            varUpp = i.upper()
+            newStr = '' 
+            for i in baseStr:
+                b = baseStr.index(i)
+                if b < n:  # набираю n символов из строки
+                    if i not in newStr:  # проверяю чтоб не добавлялись повторяющиеся символы
+                        newStr += i
+            return newStr
+
+            varLow = i.lower()
+
+    f = open('files_outputUppper.txt', 'w')  # открытие в режиме записи
+    f.write(varUpp + '\n')  # запись в файл
+    f = open('files_outputLower.txt', 'w')  # открытие в режиме записи
+    f.write(varLow + '\n')  # запись в файл
 
     f.close()  # закрытие файла
 upperAndLower("files_input5.txt")
