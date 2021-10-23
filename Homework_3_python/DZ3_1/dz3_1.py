ce1b501e1012832055b054be47c72041e8ed38f9
Sergey <sbondar05@gmail.com>
2021-10-23 10:44:39 +0300

    add homework 3.1

--- Homework_3_python/DZ3_1/dz3_1.py
+++ Homework_3_python/DZ3_1/dz3_1.py
@@ -1,16 +1,19 @@
 #def nFile(testFile):
+def readDate(filename):
 
-with open('files_input5.txt') as f:
-    n = 0
-    for line in f:
-        #if line.isnumeric():
-        #    print(type(line))
-            n += 1
-    s = str(n)
-        # line = line.strip()
-    print(s)
+    #test = open(filename)
+    with open(filename) as f:
+        n = 0
+        for line in f:
+            #if line.isnumeric():
+            #    print(type(line))
+                n += 1
+        s = str(n)
+            # line = line.strip()
+        print(s)
 
-    f = open('files_output1.txt','w')  # открытие в режиме записи
-    f.write('в файле ' + s + ' чисел')  # запись в файл
+        f = open('files_output1.txt','w')  # открытие в режиме записи
+        f.write('в файле ' + s + ' чисел')  # запись в файл
 
-f.close()  # закрытие файла
+    f.close()  # закрытие файла
+readDate("files_input1.txt")
--- Homework_3_python/DZ3_2/dz3_2.py
+++ Homework_3_python/DZ3_2/dz3_2.py
@@ -1,19 +1,15 @@
 #def nFile(testFile):
+def upperAndLower(filename):
+    with open(filename) as f:
+        c = 0
+        for line in f:
+            varUpp = line.upper()
+            f = open('files_outputUppper.txt', 'w')  # открытие в режиме записи
+            f.write(varUpp + '\n')  # запись в файл
+            varLow = line.lower()
+            f = open('files_outputLower.txt', 'w')  # открытие в режиме записи
+            f.write(varLow + '\n')  # запись в файл
 
-with open('files_input5.txt') as f:
-    c = 0
-    for line in f:
-        varUpp = line.upper()
-        f = open('files_outputUppper.txt', 'w')  # открытие в режиме записи
-        f.write(varUpp + '\n')  # запись в файл
-        varLow = line.lower()
-        f = open('files_outputLower.txt', 'w')  # открытие в режиме записи
-        f.write(varLow + '\n')  # запись в файл
 
-#f = open('files_outputUppper.txt','w')  # открытие в режиме записи
-#f.write(varUpp + '\n')  # запись в файл
-#f = open('files_outputLower.txt', 'w')  # открытие в режиме записи
-#f.write(varLow + '\n')  # запись в файл
-f.close()  # закрытие файла
-
-#nFile(files_input5.txt)
+    f.close()  # закрытие файла
+upperAndLower("files_input5.txt")
