e2bf8956cd0bde581304fe78a849430448d100a3
Sergey <sbondar05@gmail.com>
2021-10-24 20:06:37 +0300

    add homework 3 v4

--- Homework_3_python/DZ3_3/dz3_3.py
+++ Homework_3_python/DZ3_3/dz3_3.py
@@ -1,23 +1,23 @@
 def numAndLen(filename):
     with open(filename) as f:
-        c = 0
-        d = 0
-        var = []#переменная для слов
-        var2 = []#переменная для букв
+
+        countLet = 0
+        countWordText = []#переменная для слов
+        countLetText = []#переменная для букв
         for line in f:
             varTemp = len(line.split())#количество слов в строке
-            var.append(varTemp)#количество слов в тексте
+            countWordText.append(varTemp)#количество слов в тексте
             for i in line:
                 if i.isalpha():
-                    d += 1#количество букв в строке
-        var2.append(d)#количество букв в тексте
-    e = sum(var2)
-    f1 = sum(var)
-    c = e / f1
-    print(f1, d, c)
+                    countLet += 1#количество букв в строке
+        countLetText.append(countLet)#количество букв в тексте
+    countLetTextRes = sum(countLetText)
+    countWordTextRes = sum(countWordText)
+    c = countLetTextRes / countWordTextRes
+    print(countWordTextRes, countLet, countLetTextRes)
     f = open('files_output5.txt','w')  # открытие в режиме записи
     s1 = str(c)
-    s2 = str(f1)
+    s2 = str(countWordTextRes)
     f.write('в файле ' + s2 + ' слов, ' + s1 + ' средняя длина слова')  # запись в файл
 
     f.close()  # закрытие файла
