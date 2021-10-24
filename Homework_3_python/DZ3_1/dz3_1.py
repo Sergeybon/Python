961b9a2a44db3a564a34266fbadf7fd531ddd0a4
Sergey <sbondar05@gmail.com>
2021-10-24 21:19:41 +0300

    add homework 3 v5

--- Homework_3_python/DZ3_1/dz3_1.py
+++ Homework_3_python/DZ3_1/dz3_1.py
@@ -2,21 +2,41 @@
 def readDate(filename):
      with open(filename) as f:
         n = 0
-        countWordText = []  # переменная для слов
+        countNum = []  # список чисел
+        countS = []
+        max = 0
+        min = 0
+        count = 0
+        n1 = 0
         for line in f:
 
             for i in line:
                 if i.isnumeric():
-                    #print(type(line))
+                    #print(type(i))
                     n += 1
+                    if int(i) > int(max):
+                        max = i
+                    if int(i) < int(min):
+                        min = i
+                    count += int(i)
+                    if int(i) % 2 == 0:
+                        n1 += 1
+        countS.append(count)
+        countNum.append(n)  # количество чисел
 
+        countSRes = sum(countS)# сумма цифр
+        countNumtRes = sum(countNum)
+        average = countSRes / countNumtRes
+        s = str(countNumtRes)
+        s1 = str(max)
+        s2 = str(min)
+        s3 = str(average)
+        s4 = str(n1)
 
-        s = str(n)
-            # line = line.strip()
-        print(s)
+        print(s, n, countSRes, average, n1)
 
         f = open('files_output1.txt','w')  # открытие в режиме записи
-        f.write('в файле ' + s + ' чисел')  # запись в файл
+        f.write('в файле ' + s + ' чисел, ' + 'наибольшее число - ' + s1 + ', наименьшее число - ' + s2 + ', среднее арифметическое - ' + s3 + ', четных чисел в файле - ' + s4)  # запись в файл
 
         f.close()  # закрытие файла
 readDate("files_input1.txt")
